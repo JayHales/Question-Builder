5e13f80b588b74802b0c2725242d23c14842e57a
JayHales <joshhales2004@gmail.com>
2021-09-05 15:05:48 +0100

    Made names for user friendly and removed tests.

--- src/server/questions/maths/number.ts
+++ src/server/questions/maths/number.ts
@@ -1,35 +1,35 @@
 import { IQuestionStorage, Question } from '../../question';
 
 export const number: IQuestionStorage = {
-    'addition': {
+    'Addition': {
         name: 'Addition',
         forumlaTemplate: (a, b) => a + b,
         questionTextTemplate: '\\( ~0~ + ~1~ \\)'
     },
-    'subtraction': {
+    'Subtraction': {
         name: 'Subtraction',
         forumlaTemplate: (a, b) => a - b,
         questionTextTemplate: '\\( ~0~ - ~1~ \\)'
     },
-    'division': {
+    'Division': {
         name: 'Division',
         forumlaTemplate: (a, b) => (a / b).toPrecision(2),
         questionTextTemplate: '\\(~0~ \\div ~1~ \\)',
         answerFormatDescription: 'Give to 2 significant figures.',
         customParameterGeneration: (i) => noZeroAt(1, i)
     },
-    'multiplication': {
+    'Multiplication': {
         name: 'Multiplication',
         forumlaTemplate: (a, b) => a * b,
         questionTextTemplate: '\\( ~0~ \\times ~1~ \\)'
     },
-    'raising to powers': {
+    'Raising to powers': {
         name: 'Raising to powers',
         forumlaTemplate: (a, b) => Math.pow(a, b).toPrecision(2),
         answerFormatDescription: 'Give to 2 significant figures.',
         questionTextTemplate: '\\( ~0~ ^ {~1~} \\)'
     },
-    'rooting': {
+    'Rooting': {
         name: 'Rooting',
         forumlaTemplate: (a, b) => Math.pow(a, 1 / b).toPrecision(2),
         answerFormatDescription: 'Give to 2 significant figures.',
--- src/server/questions/wrapper.ts
+++ src/server/questions/wrapper.ts
@@ -1,10 +1,8 @@
 import { ITopicStorage } from '../question';
 
 import { number } from './maths/number';
-import { test } from './test/test';
 
 export const topics: ITopicStorage = {
-    'test': test,
-    'number': number
+    'Number': number
 }
 
